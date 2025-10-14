1d1f753ba8992b59386433c9d767c6fc6a7437c6
fin-netizen <564242@escg.ac.uk>
2025-10-14 11:50:12 +0100

    adding more to the weapons

--- Assets/scripts/spear.cs
+++ Assets/scripts/spear.cs
@@ -1,16 +1,32 @@
+using System.Threading;
 using UnityEngine;
 
 public class spear : MonoBehaviour
 {
+    public float Timer;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Timer = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Timer -= Time.deltaTime;
+        if (Timer < 0)
+        {
+            Destroy(gameObject);
+            Timer = 5;
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        print("tag=" + collision.gameObject.tag);
+        if (collision.gameObject.tag == "spear")
+        {
+            print("i've been hit by a spear");
+        }
     }
 }
